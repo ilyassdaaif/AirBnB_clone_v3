a63fdfa86f46cf216163f9118fc0a3b13d5ff11e
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-24 14:34:24 -0700

    a

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 """
-Module that defines the index view for the API
+Create Flask app; app_view
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def api_status():
     """
+    Returns a JSON response for RESTful API health.
+    """
+    reponse = {'status': "OK"}
+    return jsonify(reponse)
+
+
+@app_views.route('/stats')
+def get_stats():
+    """
 
     """
-    return jsonify(reponse)
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+    }
+
+    return jsonify(stats)
