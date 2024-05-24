fb6f22b7aab67996d9ae389f316bf69277dacf17
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-24 20:47:05 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
+# api/v1/views/__init__.py
 from flask import Blueprint
-
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
+# Importing views to register with the blueprint
 from api.v1.views.index import *
