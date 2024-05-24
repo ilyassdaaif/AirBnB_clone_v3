c0e250ef8a2d2d6bdb39046ad264c39051d23a2c
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-24 13:13:36 -0700

    origin

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
+# api/v1/views/__init__.py
 from flask import Blueprint
-
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-
 from api.v1.views.index import *
+
+# Blueprint creation should be after all imports
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
