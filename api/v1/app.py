55e9f3a56f3339c328cbecdb544c2c4286f4c99b
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-26 00:31:12 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
-"""
-Create Flask app; and register the blueprint app_views to Flask instance app.
-"""
-import sys
-import os
-from os import getenv
-from flask import Flask, jsonify
-
-# Add the project directory to the sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
+"""app"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown_engine(exception):
-    """
-    Close the storage engine.
-    """
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
-    """
-    Handle 404 errors with a custom JSON response.
-    """
-    response = {"error": "Not found"}
-    return jsonify(response), 404
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-if __name__ == "__main__":
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(debug=True, host=HOST, port=PORT, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
