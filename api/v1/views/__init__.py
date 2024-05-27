85e2a63ff46ded83a6793e98fb07c50eba363ce4
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-27 11:42:09 -0700

    AirBnB Clone V3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-"""create blueprint"""
+"""
+create Flask app blueprint
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-<<<<<<< HEAD
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
-=======
-if app_views is not None:
-    from api.v1.views.index import *
-    from api.v1.views.states import *
-    from api.v1.views.cities import *
-    from api.v1.views.amenities import *
-    from api.v1.views.users import *
-    from api.v1.views.places import *
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
->>>>>>> 7c42ad06336f696b5d90f30c5755a66cf841d511
+from api.v1.views.amenities import *
