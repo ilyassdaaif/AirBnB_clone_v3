1a8d144b737221afb4f189748fd4773df156590a
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-28 13:39:31 -0700

    origin

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,14 @@ from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+
+
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
